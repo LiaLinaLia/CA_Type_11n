90312155538904ec065dcacfed392b8289a46920
LiaLinaLia <lina.suminaite@codeacademylt.onmicrosoft.com>
2022-08-03 16:08:38 +0300

    init(64): typing project created

--- TypeScript [63-81]/64 - tipų aprašymas/public/js/main.js
+++ TypeScript [63-81]/64 - tipų aprašymas/public/js/main.js
@@ -1,11 +1,2 @@
 "use strict";
-const add = (a, b) => a + b;
-let a = 7;
-let b = 8;
-console.log({ a, b });
-console.log({
-    'add(a, b)': add(a, b),
-    'add(3, 10)': add(3, 10),
-});
-console.log('labas');
 //# sourceMappingURL=main.js.map
--- TypeScript [63-81]/64 - tipų aprašymas/src/main.ts
+++
@@ -1,9 +0,0 @@
-const numbers: number[] = [1, 2, 3, 4, 5, 6];
-
-// const doubleNumbers = numbers.map(x => {
-//  return x * 2;
-// });
-// pvz: const adsfsg = () => /* return */ x *2;  //eslint sako, kad kai aprašant lambda išraiška aprašoma tik viena komanda, return yra numanomas ir tos vienos komandoos rezultatas automatiškai grąžinamas, dėl to galima susitrumpinti užrašymą
-
-const doubleNumbers = numbers.map((num) => num * 2);
-console.log(doubleNumbers);
